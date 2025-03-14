16ce5ec61f7220031b8aebbde4755dbee0af617f
Aaditya Shah <aadishah1020@gmail.com>
2025-03-14 17:03:46 -0400

    Learning Node.js

--- math.js
+++ math.js
@@ -1,11 +1,11 @@
-exports.sum = (a, b) =>  a+b;
-exports.mul = (a,b) => a*b;
-exports.g = 9.8;
-exports.PI = 3.14;
+// exports.sum = (a, b) =>  a+b;
+// exports.mul = (a,b) => a*b;
+// exports.g = 9.8;
+// exports.PI = 3.14;
 
 
 // module.exports = 12345; //valid
-exports = 5; //invalid 
+// exports = 5; //invalid 
 // let obj  = {sum: sum, mul : mul, g : g, PI: PI};
 
 // obj.name = "Aaditya";
@@ -14,3 +14,9 @@ exports = 5; //invalid
 // console.log(obj);
 
 
+
+export const sum = (a, b) =>  a+b;
+export const mul = (a,b) => a*b;
+export const g = 9.8;
+export const PI = 3.14;
+//it will be exported individually and not in the form of object
--- script.js
+++ script.js
@@ -10,12 +10,12 @@
 //     console.log("Hello " , process.argv[i]);
 // }
 
-const math = require("./math");
-const fruits = require("./Fruits"); //it will go search for index.js
+// const math = require("./math");
+// const fruits = require("./Fruits"); //it will go search for index.js
 
 
-console.log(math.sum(1, 2));
-console.log(fruits.apple.color)
+// console.log(math.sum(1, 2));
+// console.log(fruits.apple.color)
 
 //NPM
 /**
@@ -35,3 +35,68 @@ console.log(fruits.apple.color)
  * DEPENDENCY - if you use API in your code then the API is the dependency for your project or code because if API fails your project fails
  * 
  */
+
+
+  //-> we cannot use packages from other directory, because it is installed locally in another directory.
+//And local installation is the best practice but we can install packages globally
+
+//import
+import {sum, PI} from "./math.js" //in require we didn't write the file name with extension but here we have to specify the extension
+
+console.log(sum(1,2))
+console.log(PI)
+
+/**
+ * To import one major thing you need to do isL
+ * The directory from which you are importing or exporting from:
+ * You need to create a package.json by "npm init"
+ * And in that package.json file you need to add:
+ * "type": "module"
+ */
+
+
+/**
+ * Reason for why to do this:
+ * By default, Node.js uses CommonJS (require) for 
+ * importing/exporting modules. If you want to use 
+ * ES Modules (import/export), you must tell Node.js explicitly
+ * by adding this line in package.json:
+{
+  "type": "module"
+}
+ */
+
+/**
+ * You cannot mix both require() and import/export, else will
+ * throw ERROR
+ */
+
+/**
+ * MAJOR ADVANTAGE OF USING IMPORT OVER REQUIRE():
+ * Selectively load only pieces we need in import
+ * LOADING IS SYNCHRONOUS for "REQUIRE" but can be :
+ * "ASYNCHRONOUS for "IMPORT" -> you can use await and async stuffs here
+ */
+
+// let figlet = require("figlet");
+//lets try using import now
+import figlet from "figlet"; //how to import depends on the package provider so always check the documentation 
+
+//we don't need to write the path like ./ for packages
+
+figlet("Aaditya Shah!!", function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  }); 
+
+  //using a random number generator package with import
+  import random from 'random'
+
+// quick uniform shortcuts
+let min = 0;
+let max = 1;
+console.log(random.float((min), (max)));
